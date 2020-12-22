0008659a62ec5d4ed4806b1cd6b8d536eabb25df
Basti <Reiss>
2020-12-22 20:38:18 +0100

    added layover
    + layover
    + responsive
    + type
    + fonts

--- src/App.js
+++ src/App.js
@@ -3,9 +3,10 @@ import React from 'react';
 import logo from './logo.svg';
 import './App.css';
 import Vimeo from '@u-wave/react-vimeo';
-//import MuteBtn from './components/MuteBTN';
-//import BTN_muted from './BTN_muted.svg';
-//import BTN_unmuted from './BTN_unmuted.svg';
+import text from './text/Text.json'
+// import MuteBtn from './components/MuteBTN';
+import wien_logo from './static/assets/wien_logo.jpg';
+import vba_logo from './static/assets/vba_logo.jpg';
 
 //
 class App extends React.Component {
@@ -16,21 +17,20 @@ class App extends React.Component {
       videoId: 489776978,
       volume: 0,
       muted: true,
-      active: false
+      active: false,
+      info_show: false
     };
+    this.text = text;
+    console.log(text);
 /*
 video IDs:
-test: 76979871
+test: 171084582
 civaTrailer: 489776978
 */
     this.handleMuted = this.handleMuted.bind(this);
     this.handleVolume = this.handleVolume.bind(this);
+    this.handleInfo =this.handleInfo.bind(this);
 
-    /*
-    this.handlePause = this.handlePause.bind(this);
-    this.handlePlayerPause = this.handlePlayerPause.bind(this);
-    this.handlePlayerPlay = this.handlePlayerPlay.bind(this);
-*/
     //props end
   }
   handleMuted(event) {
@@ -39,9 +39,10 @@ civaTrailer: 489776978
       muted: !this.muted,
     });
   }
+
   handleVolume(event) {
 
-    if(this.state.volume == 0){
+    if(this.state.volume === 0){
       this.setState({
         /*volume: parseFloat(event.target.value),*/
         volume: 0.9,
@@ -53,7 +54,11 @@ civaTrailer: 489776978
       });
     }
     this.setState({active: !this.state.active});
-    console.log(this.state.active);
+    console.log("mute");
+  }
+  handleInfo(event){
+    this.setState({info_show: !this.state.info_show});
+    console.log("info");
   }
 
 
@@ -64,23 +69,62 @@ civaTrailer: 489776978
 
 
   render (){
-    const { videoId, muted, volume,active } = this.state;
+    const { videoId, muted, volume,active,info_show} = this.state;
     return(
     <div className="App">
-      <div class="videowrapper">
-      <Vimeo  volume={volume} muted={muted} width="500" height="281" background="1" frameborder="0" 
-        video={videoId} controls="false" title="false" sidedock="false" allow="autoplay; fullscreen" loop />
+      <div className="videowrapper">
+      <Vimeo volume={volume} muted={muted} width="500" height="281" background="1" frameborder="0" 
+        video={videoId} controls="false" title="false" sidedock="false" allow="autoplay; fullscreen" loop inline />
       </div>
-      <div class="flex controls">
-        <div class="m-auto">
+      <div id="overlay" className={ (info_show ? 'visible' : 'hidden')}>
+      <div id="info_content" className="flex flex-wrap -mx-1 overflow-hidden lg:-mx-4 xl:-mx-3 ">
+        <div id="info_left" className="my-1 px-1 w-full overflow-hidden lg:my-4 lg:px-4 lg:w-1/2 xl:my-3 xl:px-3 xl:w-1/2 animate-fadeIn">
+    <h1 id="info_headline" className="">
+      {text.text_mobile.h1}</h1>
+          <h2>
+          {text.text_mobile.subh1}
+          </h2>
+          <h2 id="info_date" className="">
+          {text.text_mobile.date}
+          </h2>
+        </div>
+        <div id="info_right" className="my-1 px-1 w-full overflow-scroll lg:my-4 lg:px-4 lg:w-1/2 xl:my-3 xl:px-3 xl:w-1/2 ">
+          <h1 id="info_caption" className="">
+          {text.text_mobile.caption}
+          </h1>
+          <p id="info_text" className="">
+            {text.text_mobile.main}
+          </p>
+          <h1 id="info_caption2" className="">
+          {text.text_mobile.caption2}
+          </h1>
+          <div>
+            <div>
+            <span className="text-3l">sponsored by Wien Kultur</span>
+            <span className="text-3l">supported by VBA</span>
+            </div>
+            <div className="partner_images">
+            <img className="logo" src={wien_logo} alt="wien_logo"></img>  <img className="logo" src={vba_logo} alt="wien_logo"></img>  
+            </div>
+          </div>
+        </div>
+        </div>
+      </div> 
+      <div id="nav" className="flex controls nav">
+        <div className="m-auto">
         <div id="player" className="grid grid-cols-3 gap-2 place-content-center">
           <a href="#" className={'speaker ' + (active ? 'umute' : 'mute')}
              onClick={this.handleVolume} >
               <span></span>
           </a>
+          <div id="BTN_info" className="grid grid-cols-3 gap-2 justify-right">
+          <a href="#" className={"BTN"} onClick={this.handleInfo} >
+              <span className={ (info_show ? 'strike' : 'nostrike')}>info</span>
+          </a>
           </div>
         </div>
         </div>
+      </div>
       </div>
   );
   }
